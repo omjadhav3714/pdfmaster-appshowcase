b2ab6bc0b42ef53dc382a9ba0e71ca27c18a6fe4
ShwetaP21 <shweta.pachpute@somaiya.edu>
2022-07-19 20:29:33 +0530

    Features added

---
+++ pdfmaster/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import React from 'react';
+import Footer from './Components/Footer'
+import Features from './Components/Features'
+
+function App() {
+  return (
+    <>
+   
+    <Features/> 
+    <Footer/> 
+   
+    </>
+  );
+}
+
+export default App;
